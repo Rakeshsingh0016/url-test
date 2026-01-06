70e9ccc9f431966adff588f42c3533c50d437eb5
Rakeshsingh0016 <clickderas@gmail.com>
2026-01-06 19:25:47 +0530

    Update stats.js

--- js/stats.js
+++ js/stats.js
@@ -1,1 +1,12 @@
+import { db } from "./firebase.js";
+import { doc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const code = new URLSearchParams(location.search).get("code");
+if (!code) alert("Code missing");
+
+onSnapshot(doc(db, "links", code), (snap) => {
+  if (snap.exists()) {
+    document.getElementById("c").innerText = snap.data().clicks;
+  }
+});
 
