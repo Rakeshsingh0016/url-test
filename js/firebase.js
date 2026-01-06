03143d691ae1ced91c0bfdfd5d2f07dee0005d8c
Rakeshsingh0016 <clickderas@gmail.com>
2026-01-06 19:21:38 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,8 +1,15 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const firebaseConfig = { /* tumhara config */ };
+const firebaseConfig = {
+  apiKey: "AIzaSyAU_1WReKhyGqdWBJGaaN97rjzsyHcXHdU",
+  authDomain: "url-short-450df.firebaseapp.com",
+  projectId: "url-short-450df",
+  storageBucket: "url-short-450df.firebasestorage.app",
+  messagingSenderId: "861547833606",
+  appId: "1:861547833606:web:ff4f86c044c0b7e4e01687"
+};
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
