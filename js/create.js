8f91f5a67672a85a38738dd30e5fc780a204b634
Rakeshsingh0016 <clickderas@gmail.com>
2026-01-06 19:23:10 +0530

    Update create.js

--- js/create.js
+++ js/create.js
@@ -1,1 +1,24 @@
+import { db } from "./firebase.js";
+import { doc, setDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+function genCode() {
+  return Math.random().toString(36).substring(2, 8);
+}
+
+window.create = async function () {
+  const longUrl = document.getElementById("url").value;
+  if (!longUrl) return alert("URL required");
+
+  const code = genCode();
+
+  await setDoc(doc(db, "links", code), {
+    longUrl,
+    clicks: 0,
+    disabled: false,
+    createdAt: new Date()
+  });
+
+  document.getElementById("result").innerText =
+    location.origin + "/" + code;
+};
 
